64d23d5b94455c64c49eefc8b613e2e0688c2b17
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-02-25 10:30:25 +0100

    added homeworks

---
+++ homeworks/module0.py
@@ -0,0 +1,46 @@
+# Lecture 0
+
+# 0.0
+# Create a variable that checks if a year is a leap year
+# A year is a leap year if it's:
+# - divisible by 4 AND
+# - not divisible by 100, unless it's also divisible by 400 (eg 1700 is not a leap year, 1600 is)
+# Test the results with the following years:
+year_0 = 1900
+year_1 = 2000
+year_2 = 2024
+year_3 = 2025
+
+year_to_test = year_0  # change this to test other years
+
+is_leap_year = ...
+
+
+
+# 0.1
+# Create a temperature converter that:
+# 1. Takes a temperature in Fahrenheit (use float)
+# 2. Converts it to Celsius (google the formula)
+# transforming the float result into an int
+# 3. Define variables telling you if water would:
+#    - freeze (below 0°C)
+#    - boil (100°C or above)
+#    - or be liquid (between 0-100°C)
+# Test with these temperatures:
+temp_0 = 32.0    
+temp_1 = 212.0   
+temp_2 = 98.6    
+temp_3 = -40.0
+
+temp_to_test = temp_0  # change this assigment to test other temperatures
+
+temp_in_celsius = ...  # make sure this is an int!
+is_frozen = ...
+is_boiling = ...
+is_liquid = ...   # ensure that only one of these is true for each of the possible temperatures!
+
+
+
+
+
+
