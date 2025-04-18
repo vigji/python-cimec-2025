8ed507d9cb486897bf96804dd9575b89eae25693
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-04-18 19:53:06 +0200

    post lecture 1.1

--- homeworks/module1.py
+++ homeworks/module1.py
@@ -1,3 +1,4 @@
+# %%
 #############
 # Lecture 1.0
 #############
@@ -68,3 +69,59 @@ img = fetch_image()  # print its shape to know the number of pixels
 # To do so, initialize an empty (w,h,3) array and use indexing to fill with the image values the correct
 # channels in different parts of the image. Bonus points: do it with a loop.
 # After you have done it, consider this question: were you working inplace or on copies?
+
+
+
+########################
+# Lecture 1.1
+########################
+
+# 1.1.0
+# Let's look at some real neurons! The following function will download some microscopy data
+# from a calcium imaging experiment (data from https://data.mendeley.com/datasets/hjgtsmyyv5/1)
+# Run the following cell to load the data:
+
+
+def fetch_image():
+    """Fetch exercise data from github repo."""
+
+    import numpy as np
+    import requests
+    from io import BytesIO
+    URL = "https://github.com/vigji/python-cimec-2025/raw/main/practicals/data/calcium_data.npy"
+    
+    return np.load(BytesIO(requests.get(URL).content))
+
+#  The data is a 3D array: first dimension is time, second and third are space (height, width).
+#  Each value in the array is the fluorescence level of the pixel.
+
+# 1. Plot the first frame using plt.imshow. You'll see a lot of blobs, or doughnuts.
+# These are the neurons! They are labelled with a fluorescent dye. When they activate,
+# fluoresce in the cell increases.
+
+# 2. Single frames will be noisy! Plot the mean of the first 100 frames!
+
+# 3. Let's try to look at what neurons are more active. Compute the maximum projection
+# over time to locate the cells with more activity!
+
+# 4. Many times, we want to normalize the fluorescence levels over time to compare activity 
+# across pixels, independently of their absolute fluorescence levels.
+# To do so, for every pixel, we can:
+# - compute a baseline value; in our case, we can use the mean over time;
+# - subtract for every timepoint the baseline value from the timepoint fluorescence;
+# - divide the result by the baseline value.
+
+# The normalized signal is called DeltaF / F:
+# delta_f_f = (f - mean_f) / mean_f 
+
+# Use broadcasing  to compute delta_f_f from the stack, without using for loops!
+
+# 5. # Let's now look at some signals over time. Locate the region with more activity, or a neuron,
+# and plot the average activity over time for that region! If you don't know where to look for,
+# the area covered by vertical_lims = [25, 45] and horizontal_lims = [0, 20] has nice signal!
+
+# 6. Let's say we want to make a boolean mask for the neurons. One way of creating such mask
+# can be to use a threshold to find the pixels whose maximum projection is above a certain value.
+# Try to compute such mask, playing with the threshold value until you find a number that 
+# works to isolate the neurons (they will appear as some 15-20 blobs in the image).
+# You can plot the boolean mask using matplotlib to see how it looks like.
